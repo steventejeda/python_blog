c94bc767beff58eb092b34984bcb6d2abbb60dfc
steventejeda <steventejeda@gmail.com>
2021-08-28 20:57:16 -0400

    Restricting page access

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
 import sys
 sys.dont_write_bytecode = True
 
@@ -6,5 +8,6 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/home")
+@login_required
 def home():
     return render_template("home.html", name="Steven") 
